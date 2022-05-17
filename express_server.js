1f6f81a007aea5de4a29abd00557a5b26a530b23
JorLo <jory.laurin@gmail.com>
2022-05-17 15:05:35 -0700

    added ejs

--- express_server.js
+++ express_server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "ejs");
+app.set('view engine', "ejs");
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -21,12 +26,12 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-app.get("/set", (req, res) => {
+app.get('/set', (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
  });
  
-app.get("/fetch", (req, res) => {
+app.get('/fetch', (req, res) => {
   res.send(`a = ${a}`);
  });
 
